8af4248fa4d838396a0917e37fdf4673a7a19c53
Limekee <142234221+Limekee@users.noreply.github.com>
2026-02-16 16:31:30 +0500

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, model_validator
 from sqlalchemy import text
 from db import engine
 from uuid import uuid4, UUID
@@ -10,22 +10,32 @@ class Zone(BaseModel):
     id: UUID
     park_id: UUID
     name: str
-    free: int
-    total:int
+    free: int = Field(ge=0)
+    total:int = Field(gt=0)
+
+    @model_validator(mode='after')
+    def check_total_gt_free(self):
+        if self.total <= self.free:
+            raise ValueError('поле total должно быть больше чем поле free')
+        return self
 
 class ParkingLot(BaseModel):
     id: UUID 
     name: str
     zones: List[Zone]
-
+    description : str
+    last_update: str
+    
 
 
 def append_parking_lot(parking_lot:ParkingLot):
     with engine.connect() as conn:
         with conn.begin():
             conn.execute(
-                text("INSERT INTO parking_lots (id, name) VALUES (:id, :name)"),
-                {"id": parking_lot.id, "name":parking_lot.name}
+                text("INSERT INTO parking_lots "
+                "(id, name, description, last_update) " \
+                "VALUES (:id, :name, :description, NOW())"),
+                {"id": parking_lot.id, "name":parking_lot.name, "description":parking_lot.description}
             )
 
             prompt = """
@@ -69,6 +79,8 @@ def get_all() -> dict:
     SELECT 
         park.id::text,
         park.name,
+        park.description,
+        park.last_update::text,
         zone.id::text,
         zone.name,
         zone.free,
@@ -85,21 +97,28 @@ def get_all() -> dict:
             park_dict = {}
             for zone in data:
                 app_zone = {
-                    "zone_name":zone[3],
-                    "zone_free":zone[4],
-                    "zone_total":zone[5]
+                    "zone_name":zone[5],
+                    "zone_free":zone[6],
+                    "zone_total":zone[7]
                     }
                 if zone[0] in park_dict:
                     park_dict[zone[0]]["zones"].append(app_zone)
                 else:
-                    park_dict[zone[0]] = {"park_name": zone[1], "zones":[app_zone]}
+                    park_dict[zone[0]] = {
+                        "park_name": zone[1],
+                        "description": zone[2],
+                        "zones":[app_zone],
+                        "last_update":zone[3]
+                        }
             return park_dict
 
 def get_park_lot(park_lot_name) -> ParkingLot:
-    promt = """
+    prompt = """
     SELECT
         parking.id::text,
         parking.name,
+        parking.description,
+        parking.last_update::text,
         zone.id::text,
         zone.name,
         zone.free,
@@ -109,12 +128,12 @@ def get_park_lot(park_lot_name) -> ParkingLot:
     WHERE parking.name = :namee
     """
     with engine.connect() as conn:
-        result = conn.execute(text(promt), {"namee":park_lot_name})
+        result = conn.execute(text(prompt), {"namee":park_lot_name})
 
         data = result.fetchall()
 
         ####### обработка данных для отправки######
-        zone_list = [Zone(id=zone[2], 
+        zone_list = [Zone(id=zone[-4], 
                           park_id=data[0][0], 
                           name=zone[-3],
                           free=zone[-2],
@@ -122,7 +141,12 @@ def get_park_lot(park_lot_name) -> ParkingLot:
                           ) 
                      for zone in data
                      ]
-        parking_lot = ParkingLot(id=data[0][0], name=data[0][1], zones=zone_list)
+        parking_lot = ParkingLot(
+            id=data[0][0], 
+            name=data[0][1],
+            description=data[0][2],
+            last_update=data[0][3], 
+            zones=zone_list)
         return parking_lot
 
 def update_parking_lot_free_value(parking_lot: ParkingLot):
@@ -145,14 +169,24 @@ def update_parking_lot_free_value(parking_lot: ParkingLot):
 # park_id1, park_id2 = uuid4(), uuid4()
 
 # zone1 = Zone(id=uuid4(), park_id=park_id1, name="центр", free=80, total=100)
-# zone2 = Zone(id="b2e12384-e640-4046-bbe4-0816dd7a9077", park_id=park_id2, name="север", free=5, total=120)
-# zone3 = Zone(id="9a3a5294-e827-4497-8bf7-f3f8090ba22e", park_id=park_id2, name="центр", free=5, total=80)
-# zone4 = Zone(id="b76af911-ba49-426e-a7b9-b4fb4c044056", park_id=park_id2, name="юг", free=5, total=100)
+# zone2 = Zone(id=uuid4(), park_id=park_id2, name="север", free=5, total=120)
+# zone3 = Zone(id=uuid4(), park_id=park_id2, name="центр", free=5, total=80)
+# zone4 = Zone(id=uuid4(), park_id=park_id2, name="юг", free=5, total=100)
 
-# parking_lot1 = ParkingLot(id=park_id1, name="ИРИТ-РТФ", zones=[zone1])
-# parking_lot2 = ParkingLot(id=park_id2, name="ГРИНВИЧ", zones=[zone2, zone3, zone4])
+# parking_lot1 = ParkingLot(
+#     id=park_id1, 
+#     name="ИРИТ-РТФ", 
+#     zones=[zone1], 
+#     description="парковка великого РТФ шампиньона",
+#     last_update="вчера"
+#     )
+# parking_lot2 = ParkingLot(
+#     id=park_id2, 
+#     name="ГРИНВИЧ", 
+#     zones=[zone2, zone3, zone4],
+#     description="крутая такая",
+#     last_update="вчера"
+#     )
 
 # append_parking_lot(parking_lot1)
 # append_parking_lot(parking_lot2)
-
-#pprint.pprint(get_all())
